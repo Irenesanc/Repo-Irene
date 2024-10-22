347d417f29370a0219255e69f7896a0a33747fee
Irenesanc <isancgir@myuax.com>
2024-10-22 18:38:02 +0200

    Update 05.loanpayments.py

--- 05.loanpayments.py
+++ 05.loanpayments.py
@@ -1,7 +1,5 @@
 # Monthly Loan Payments variables:
 
-import pandas as pd
-
 #P is the principal loan ammount
 P=float(input("Enter the principal loan ammount: "))
 #r is the monthly interest rate
@@ -9,15 +7,7 @@ r=float(input("Enter the monthly interest rate (annual interest divided by 12 mo
 #n is the loan termns
 n=float(input("Enter the loan terms (in months): "))
 
-rango_n=range(1,n+1)
-
 #Monthly Loan Payments`s formula`
 M= (P*(r*(1+r)**n))/((1+r)**n-1)
 
-payments_df=pd.DataFrame({'Month':rango_n,'Payment': M})
-print(payments_df)
-
-Total_payment=M*n
-print("Total payment is",Total_payment)
-print("The monthly payment is:",M)
-
+print("The monthly payment of the loan is:",M)
